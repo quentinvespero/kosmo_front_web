abfd09c2951805258984fb2605d384e3810ca49b
quentinvespero <quentinvespero@gmail.com>
2023-10-30 17:04:38 +0100

    small twinking on style

--- src/components/FeedSelector.tsx
+++ src/components/FeedSelector.tsx
@@ -5,7 +5,6 @@ const FeedSelector = () => {
         <div className='feedSelector'>
             <div className="feedElements feed1">feed 1</div>
             <div className="feedElements feed3 feedSelectedElement">feed 2</div>
-            <div className="feedElements feed2">zefhezfhrizeuhrfeizu</div>
             <div className="feedElements feed4">feed 4</div>
             <div className="feedElements feed5">feed 5</div>
         </div>
