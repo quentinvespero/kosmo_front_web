4f6fbb6c49e7f6173e28d4a6022a2e85709db54d
quentinvespero <quentinvespero@gmail.com>
2023-12-29 08:25:14 +0100

    slight changes in text

--- src/screens/EntryPage.tsx
+++ src/screens/EntryPage.tsx
@@ -55,10 +55,10 @@ const EntryPage: React.FC<EntryPageProps & ScreenProps> = ({ pageSelection, anim
                     <div className="entryPage-description">
                         <p>Get into the Field.</p>
                         <br />
-                        <p>Kosmo aims to be a quality-focused social network, providing a comfortable and enriching space.</p>
+                        <p>Kosmo aims to be a quality-focused social network, that provide a comfortable and enriching space.</p>
                         <p>A space where your time would feel well-spent.</p>
                         <br />
-                        <p>By putting humans first in my reflections, I designed the features to encourage online civility and reduce online influence and hatred.</p>
+                        <p>By putting humans first in my reflections, I designed the features to encourage online civility and reduce influence and hatred.</p>
                     </div>
                 </div>
             </div>
