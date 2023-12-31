b766fff6f632f6c31246d5923106e52f0e7ad7d3
quentinvespero <quentinvespero@gmail.com>
2023-12-31 01:36:06 +0100

    connected the feed selector to the json

--- src/components/FeedSelector.tsx
+++ src/components/FeedSelector.tsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FeedSelectorProps, ScreenProps } from '../interfaces/interfaces'
+import jsonData from '../../src/assets/bdd.json'
 
-const FeedSelector:React.FC<FeedSelectorProps & ScreenProps> = ({ screenFormat, topmenuIsSticky }) => {
+const FeedSelector: React.FC<FeedSelectorProps & ScreenProps> = ({ screenFormat, topmenuIsSticky }) => {
+
+    const [selectedFeed, setSelectedFeed] = useState('feed1')
+
+    const handleFeedChoice = (feedId: string) => {
+        setSelectedFeed(feedId)
+    }
+
     return (
-        // <div className='feedSelector' style={{ flexWrap: screenFormat==='mobile' && topmenuIsSticky ? 'unset' : 'wrap'}}>
-        <div className='feedSelector' style={{ 
-            flexWrap: screenFormat==='mobile' && !topmenuIsSticky ? 'wrap' : 'unset',
+        <div className='feedSelector' style={{
+            flexWrap: screenFormat === 'mobile' && !topmenuIsSticky ? 'wrap' : 'unset',
             gap: topmenuIsSticky ? '.2rem' : '.7rem'
         }}>
             <div className="feedElements feedElements-profile">{screenFormat != 'mobile' && 'Profile'}</div>
-            <div className="feedElements feedElements-1 feedElements-selected">Feed 1</div>
-            <div className="feedElements feedElements-2">Feed 2</div>
-            <div className="feedElements feedElements-3">Feed 3</div>
+            {jsonData && 
+            jsonData.feeds.length > 0 &&
+            jsonData.feeds.map((feed) => (
+                <div
+                    key={feed._id}
+                    className={`feedElements ${feed._id} ${selectedFeed === feed._id ? 'feedElements-selected' : ''}`}
+                    onClick={() => handleFeedChoice(feed._id)}
+                >
+                    {feed.name}
+                </div>
+            ))}
         </div>
     )
 }
