45cc841349100c977eff304a0c18e35d1a4c17f8
quentinvespero <quentinvespero@gmail.com>
2024-01-31 17:55:21 +0100

    improving layout in headerMenu

--- src/components/header/HeaderMenu/AddPost.tsx
+++ src/components/header/HeaderMenu/AddPost.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ButtonWithIcon from '../../buttons/ButtonWithIcon'
+import DraftSection from './DraftSection'
 
 const AddPost = () => {
+
+    const [addPostSelectedSection, setAddPostSelectedSection] = useState<'photo'|'video'|'text'|'poll'|'draft'>('text')
 
     const textMaxLength = 500
 
     return (
         <div className='addPost'>
             <div className="addPost-addMediaButtons">
-                <ButtonWithIcon buttonIcon='icon_photo_white2' buttonText='addPostSection-photo'/>
-                <ButtonWithIcon buttonIcon='icon_video_white2' buttonText='addPostSection-video'/>
-                <ButtonWithIcon buttonIcon='icon_poll_white2' buttonText='addPostSection-poll'/>
+                <ButtonWithIcon buttonIcon='icon_photo_white2' buttonText='addPostSectionButton-photo'/>
+                <ButtonWithIcon buttonIcon='icon_video_white2' buttonText='addPostSectionButton-video'/>
+                <ButtonWithIcon buttonIcon='icon_poll_white2' buttonText='addPostSectionButton-poll'/>
             </div>
             <div className="addPost-elements">
                 <textarea 
@@ -20,6 +23,7 @@ const AddPost = () => {
                     // value={postText}
                     maxLength={500}
                 ></textarea>
+                <DraftSection/>
                 <div className="addPost-elements-buttons">
                     <ButtonWithIcon buttonIcon='icon_drafts_white2' buttonText='addPostSectionBottomDraft-drafts'/>
                     <ButtonWithIcon buttonIcon='icon_arrow3_white2' buttonText='addPostSectionBottomPost-Post'/>
