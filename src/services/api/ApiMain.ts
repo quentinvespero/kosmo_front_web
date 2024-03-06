55bf36906bd013cd3caac54dc32f831919aa2a33
Quentin Vespero <quentinvespero@gmail.com>
2024-03-06 02:12:01 +0100

    testing new react feature for fetching API

--- src/services/api/ApiMain.ts
+++ src/services/api/ApiMain.ts
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
-const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_apiUrl,
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
+// const axiosInstance = axios.create({
+//     baseURL: import.meta.env.VITE_apiUrl,
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// })
+
+const {data} = use(axios.get(`${}`))
