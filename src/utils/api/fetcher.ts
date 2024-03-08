972d792f08afc48e1700a5ce19e2a56d1213ea0a
Quentin Vespero <quentinvespero@gmail.com>
2024-03-08 23:55:29 +0100

    working on API/backend data exchange features

--- src/utils/api/fetcher.ts
+++ src/utils/api/fetcher.ts
@@ -1,10 +1,15 @@
 // here, baseUrl contains the value stored in the environment variables, since i'm using VITE, it starts with VITE_
 const baseUrl = `${import.meta.env.VITE_API_URL}:${import.meta.env.VITE_API_PORT}`
 
-// simple fetch operation that will be used in the requests below
-const fetcher = (url:string) => fetch(`${baseUrl}${url}`).then((response) => response.json())
+// basic fetch used as the base for the requests below
+// const fetcher = (url:string) => fetch(`${baseUrl}${url}`).then((response) => response.json())
+const baseFetch = async (url: string, options?: RequestInit) => {
 
-// put
-export const get = (url: string) => fetcher(url)
+}
+
+// get request (that use fetcher above)
+export const get = (url: string) => baseFetch(url)
+
+
 
 export default fetcher
