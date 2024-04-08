e8502dc73d82eb3d275bd4e1a13617d90f711f1c
quentinvespero <quentinvespero@gmail.com>
2024-04-08 21:31:13 +0200

    preparing the feature to add/remove columns

--- src/components/innerSections/innerSectionHome/feedViews/FeedColumnsView.tsx
+++ src/components/innerSections/innerSectionHome/feedViews/FeedColumnsView.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FeedViewsInterfaces } from '../../../../interfaces/feedViewsInterfaces'
 import { ScreenProps } from '../../../../interfaces/interfaces'
 import Post from '../../../post/Post'
 import FeedColumn from './FeedColumn'
 import AddFeedButton from './AddFeedButton'
 
+interface Column {
+    id: string
+}
+
 const FeedColumnsView:React.FC<FeedViewsInterfaces & ScreenProps> = ({posts, screenFormat, idSelectedPost, setIdSelectedPost, selectedViewType}) => {
+
+    const [columns, setColumns] = useState<Column[]>([{ id: 'column1' }])
+
     return (
         <div className='feedViews feedColumnsView'>
             {/* <div className="test">columns</div> */}
